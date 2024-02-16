e3c846efcd30e7904de7cac074c5150531119e05
ohy9205 <ohy9205@naver.com>
2024-02-17 03:19:01 +0900

    문제 조판 화면 구현

--- app/components/TypesettingView.jsx
+++ app/components/TypesettingView.jsx
@@ -1,33 +1,123 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useSelectedExamList } from "../context/SelectedExamListContext";
-import RemoveButton from "./RemoveButton";
+import Button from "./Button";
 import TypesettingItem from "./TypesettingItem";
 
+const INNER_HEIGHT = 1122;
+const MARGIN_HEIGHT = 20;
+const META_HEIGHT = 110;
+const BUTTON_HEIGHT = 34;
+
 const TypesettingView = () => {
-  const { list } = useSelectedExamList();
+  const { list, removeItem } = useSelectedExamList();
+  const [pages, setPages] = useState([]);
+
+  // 리스트 가바뀔때마다 새로 렌더링
+  useEffect(() => {
+    const newPages = makePages(list);
+    setPages(newPages);
+  }, [list]);
 
   return (
-    <section
-      className="flex flex-grow p-4"
-      style={{ width: "calc(100vh * 0.707)", height: "calc(100vh - 1rem)" }}>
-      <div className="w-1/2 flex flex-col gap-10">
-        왼쪽단
-        {list?.map((it, idx) => {
-          return (
-            <div
-              key={it.examKey}
-              className="flex flex-col gap-4 shadow-lg bg-gray-50">
-              <TypesettingItem item={it} idx={idx} />
-              <RemoveButton examKey={it.examKey} />
+    <section className="flex flex-col gap-10">
+      {pages.map((page, pageIndex) => (
+        <div
+          key={pageIndex}
+          className="h-[1122px] flex justify-center items-center bg-slate-200">
+          <div>
+            <div className={`h-[1100px] flex`}>
+              {/* <div className={`h-[${INNER_HEIGHT}px] flex`}> */}
+              <div className="w-1/2 flex flex-col">
+                {page.left?.map((item, index) => (
+                  <div key={item.examKey} className="shadow-lg bg-gray-50 my-5">
+                    <TypesettingItem
+                      item={item}
+                      idx={countItemsBeforeIndex(pages, pageIndex) + index}
+                    />
+                    <Button action={() => removeItem(item.examKey)}>
+                      삭제하기
+                    </Button>
+                  </div>
+                ))}
+              </div>
+              <div className="w-[2px] h-full bg-slate-300"></div>
+              <div className="w-1/2 flex flex-col">
+                {page.right?.map((item, index) => (
+                  <div key={item.examKey} className="shadow-lg bg-gray-50 my-5">
+                    <TypesettingItem
+                      item={item}
+                      idx={
+                        countItemsBeforeIndex(pages, pageIndex) +
+                        page.left.length +
+                        index
+                      }
+                    />
+                    <Button action={() => removeItem(item.examKey)}>
+                      삭제하기
+                    </Button>
+                  </div>
+                ))}
+              </div>
             </div>
-          );
-        })}
-      </div>
-      <div className="border mx-2"></div>
-      <div className="w-1/2">오른쪽단</div>
+            <h3 className="text-center">{pageIndex + 1}</h3>
+          </div>
+        </div>
+      ))}
     </section>
   );
 };
+
+const makePages = (list) => {
+  const newPages = [[]];
+  let leftTotalHeight = 0;
+  let rightTotalHeight = 0;
+  let currentPageIndex = 0;
+
+  // 선택한 문제가 변경될때마다 페이지 구성을 새로함
+  list.forEach((item) => {
+    const itemHeight =
+      item.height + META_HEIGHT + BUTTON_HEIGHT + MARGIN_HEIGHT * 2;
+
+    // 페이지 유무 체크
+    if (newPages[currentPageIndex].length === 0) {
+      newPages[currentPageIndex] = { left: [], right: [] };
+    }
+
+    if (
+      // 왼쪽 단
+      newPages[currentPageIndex].right.length === 0 &&
+      leftTotalHeight + itemHeight < INNER_HEIGHT
+    ) {
+      newPages[currentPageIndex].left.push(item);
+      leftTotalHeight += itemHeight;
+    } else if (rightTotalHeight + itemHeight < INNER_HEIGHT) {
+      // 오른쪽 단
+      newPages[currentPageIndex].right.push(item);
+      rightTotalHeight += itemHeight;
+    } else {
+      // 다음 페이지 생성
+      currentPageIndex++;
+      newPages[currentPageIndex] = { left: [item], right: [] };
+      leftTotalHeight = itemHeight;
+      rightTotalHeight = 0;
+    }
+  });
+  return newPages;
+};
+
+function countItemsBeforeIndex(arr, index) {
+  // 합산할 총 항목 수 초기화
+  let totalCount = 0;
+
+  // 주어진 인덱스 이전까지 반복하여 각 항목의 left와 right 배열의 길이를 더함
+  for (let i = 0; i < index; i++) {
+    totalCount += arr[i].left.length + arr[i].right.length;
+  }
+
+  // 계산된 총 항목 수 반환
+  return totalCount;
+}
 
 export default TypesettingView;
